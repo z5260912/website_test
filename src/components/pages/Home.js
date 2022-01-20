f49866d287d68ba0032e06fb91a0187031a69d4f
jiayi32 <71424312+jiayi32@users.noreply.github.com>
2022-01-20 21:47:02 +1100

    initial commit for you john

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactPlayer from 'react-player'
+import '../../App.css';
+import HeroSection from '../HeroSection';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
